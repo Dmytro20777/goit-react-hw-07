94449f78fe88f535b2ac645467d275be6be5e285
Dmytro20777 <08dmutro11@gmail.com>
2024-02-27 10:13:51 +0200

    Homework07

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactForm from "./components/ContactForm/ContactForm";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchContacts } from "./redux/operations";
+import { selectContacts } from "./redux/selectors";
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const { loading, error } = useSelector(selectContacts);
+
+    useEffect(() => {
+    dispatch(fetchContacts())
+  }, [dispatch])
+
+
+
   return (
     <div style={{ maxWidth: "1400px", margin: "0 auto"}}>
       <h1 style={{ textAlign: 'center' }}>Phonebook</h1>
       <ContactForm/>
-      <SearchBox/>
+      <SearchBox />
+        {loading && <p>Loading...</p>}
+        {error && <p>Error</p>}
       <ContactList/>
     </div>
   );
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,11 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Contact } from "../Contact/Contact";
 import css from "./ContactList.module.css"
-import { useEffect } from "react";
-import { selectContacts, visibleContacts } from "../../redux/selectors";
-import { deleteContact, fetchContacts } from "../../redux/operations";
+import { visibleContacts } from "../../redux/selectors";
+import { deleteContact } from "../../redux/operations";
 
 const ContactList = () => {
   const dispatch = useDispatch();
-  const { loading, error } = useSelector(selectContacts);
-
-  useEffect(() => {
-    dispatch(fetchContacts())
-  }, [dispatch])
-
   const contacts = useSelector(visibleContacts);
 
   const deleteUser = (userId) => {
@@ -23,8 +16,6 @@ const ContactList = () => {
     return (
       <div>
         <ul className={css.list}>
-        {loading && <p>Loading...</p>}
-        {error && <p>Error</p>}
         {contacts.length > 0 && contacts.map((item) => (
           <li key={item.id} className={css.item}>
             <Contact item={item} onDelete={deleteUser} />
